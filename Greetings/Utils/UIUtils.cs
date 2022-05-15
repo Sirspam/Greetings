d30294de677e0d783877ee814d8cf014d365c985
Sirspam <71392316+Sirspam@users.noreply.github.com>
2022-05-16 00:08:29 +0100

    Updated ButtonUnderline logic and values

--- Greetings/Utils/UIUtils.cs
+++ Greetings/Utils/UIUtils.cs
@@ -7,8 +7,10 @@ namespace Greetings.Utils
 {
 	internal class UIUtils
 	{
+		private Color? _defaultUnderlineColor;
 		private PanelAnimationSO? _presentPanelAnimation;
 		private PanelAnimationSO? _dismissPanelAnimation;
+
 		private readonly TimeTweeningManager _uwuTweenyManager; // Thanks once again, PixelBoom
 		private ColorsOverrideSettingsPanelController _colorsOverrideSettingsPanelController;
 		
@@ -21,14 +23,14 @@ namespace Greetings.Utils
 		public PanelAnimationSO PresentPanelAnimation => _presentPanelAnimation ??= Accessors.PresentAnimationAccessor(ref _colorsOverrideSettingsPanelController);
 		public PanelAnimationSO DismissPanelAnimation => _dismissPanelAnimation ??= Accessors.DismissAnimationAccessor(ref _colorsOverrideSettingsPanelController);
 
-		public async void ButtonUnderlineClick(GameObject gameObject)
-		{
-			var underline = await Task.Run(() => gameObject.transform.Find("Underline").gameObject.GetComponent<ImageView>());
+        public void ButtonUnderlineClick(GameObject gameObject)
+        {
+            var underline = gameObject.transform.Find("Underline").gameObject.GetComponent<ImageView>();
+            _defaultUnderlineColor ??= underline.color;
 
-			_uwuTweenyManager.KillAllTweens(underline);
-
-			var tween = new FloatTween(0f, 1f, val => underline.color = Color.Lerp(new Color(0f, 0.7f, 1f), new Color(1f, 1f, 1f, 0.502f), val), 1f, EaseType.InSine);
-			_uwuTweenyManager.AddTween(tween, underline);
-		}
+            _uwuTweenyManager.KillAllTweens(underline);
+            var tween = new FloatTween(0f, 1f, val => underline.color = Color.Lerp(new Color(0f, 0.75f, 1f), (Color) _defaultUnderlineColor, val), 0.6f, EaseType.InQuad);
+            _uwuTweenyManager.AddTween(tween, underline);
+        }
 	}
 }
